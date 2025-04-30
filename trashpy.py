d9bf9579ab2774484eed7f2908d6bfd365b760d9
ccuellarn <c.cuellarn@uniandes.edu.co>
2025-04-30 13:17:10 -0500

    fixed time in html with ra and dec graphics

--- trashpy.py
+++ trashpy.py
@@ -17,8 +17,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     datos_tabla = None
-    plot_url = None
-    plot_url_altaz = None 
+    plot_url = None 
     alertas = None
     error = None
     
@@ -85,8 +84,9 @@ def index():
             Date_f=date_f,
             time_scale=timescale,
             rango=many,
-            m_min=m_min,
             limit=limite_altitud,
+            m_min=m_min,
+
         )
         
         time_array = CreateTime(date_i, date_f, timescale)
@@ -101,13 +101,71 @@ def index():
             rango=many,
             Time=time_array,
             General_time=general_time,
-            K=K
+            K=K,
         )
-        
-        graphic = Graphic(Data=data_observations,
-                    rango=many,
-                    time=time_array)
 
+        # graphic = Graphic(data_observations, many, time_array)
+
+        # if graphic:
+        #     plt.figure(figsize=(11, 7))
+        #     ordertime = []
+        #     min_alt = []
+        #     max_alt = []
+        #     orderlabels = [i for i in order['Label'].drop_duplicates()]
+
+        #     for i in orderlabels: 
+        #         hour_i = order.loc[order['Label']==i].head(1)['Time'].iloc[0].datetime.hour
+        #         hour_f = order.loc[order['Label']==i].tail(1)['Time'].iloc[0].datetime.hour
+        #         minute_i = order.loc[order['Label']==i].head(1)['Time'].iloc[0].datetime.minute
+        #         minute_f = order.loc[order['Label']==i].tail(1)['Time'].iloc[0].datetime.minute
+
+        #         hour_i = hour_i - 24 if 13 <= hour_i <= 23 else hour_i
+        #         hour_f = hour_f - 24 if 13 <= hour_f <= 23 else hour_f
+    
+        #         ordertime.append((float(f"{hour_i}.{minute_i}"),float(f"{hour_f}.{minute_f}"))) 
+
+        #     for each in graphic:
+        #         if each['Label'].iloc[0] in orderlabels:
+
+        #             ALT = each['Alt'].tolist()
+        #             AZ = each['Az'].tolist()
+        #             T = each['Time'].tolist()
+
+        #             plt.scatter(T, ALT,c=AZ,cmap="viridis",lw=1)
+
+        #             for j, alt in enumerate(ALT):
+        #                 plt.annotate(str(each['Label'].iloc[0]), (T[j], alt + 1), color='k')        
+
+        #             min_alt.append(min(ALT))
+        #             max_alt.append(max(ALT))
+
+        #     # Limits of good observations
+        #     plt.plot(time_array, [limite_altitud] * len(time_array), '--', color='grey')
+        #     #Details
+        #     plt.colorbar().set_label("Azimuth [deg]")
+        #     plt.xlabel("Hours around Midnight")
+        #     plt.ylabel("Altitude [deg]")
+
+        #     # time
+        #     for each in ordertime:
+        #         plt.plot([each[0]] * len(np.linspace(min(min_alt),max(max_alt), len(time_array))), np.linspace(min(min_alt),max(max_alt), len(time_array)), '--', color='orange')
+        #         plt.plot([each[1]] * len(np.linspace(min(min_alt),max(max_alt),len(time_array))), np.linspace(min(min_alt),max(max_alt), len(time_array)), '--', color='salmon')
+
+        #     plt.text( each[0],min(min_alt), 'S', color='orange')
+        #     plt.text( each[1],min(min_alt), 'F',color='salmon')
+
+        #     #Exes
+        #     hours = [str(round(num + 24,1)) if num < 0 else str(round(num,1)) for num in np.array([-6,-4,-2,0,2,4,6])]
+        #     plt.xticks(np.array([-6,-4,-2,0,2,4,6]), hours)
+        #     plt.title('Starting time {}\n Location: ({} , {})'.format(date_i,observer[0],observer[1]))
+
+        # # Convertir la gráfica a imagen base64
+        # buffer = BytesIO()
+        # plt.savefig(buffer, format='png')
+        # buffer.seek(0)
+        # plot_url = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        # plt.close()  # Cerrar la figura para liberar memoria
+            
         if isinstance(order, pd.DataFrame) and not order.empty:
             # Convertir el DataFrame a un formato adecuado para la plantilla
             datos_tabla = []
@@ -122,99 +180,17 @@ def index():
                     'tiempo_exposicion': f"{row['Time expo']:2f}s"
 
                 })
-            names = [i for i in order['Name'].drop_duplicates()]
-            ra = [Angle(i, unit=u.deg).degree for i in order['RA'].drop_duplicates()]
-            dec = [Angle(i, unit=u.deg).degree for i in order['DEC'].drop_duplicates()]
-
-            plt.figure(figsize=(9, 7))
-
-            plt.title('Starting time {}\n Location: ({} , {})'.format(date_i,observer[0],observer[1]))
-
-            plt.scatter(ra,dec,color='purple',marker='*', s=80)
-            for i in range(0, len(dec)):
-                plt.text(ra[i], dec[i], '{}) {}'.format(str(i+1), str(names[i])), color='k')
-    
-            plt.ylabel(r"Declination [deg]",fontsize=14)
-            plt.xlabel(r'Right ascension [deg]',fontsize=14)
-            plt.grid(True)
-            plt.tight_layout()
-            # # Generar la gráfica
-            # plt.figure(figsize=(11,7))
-
-            # ordertime = []
-            # min_alt = []
-            # max_alt = []
-            # orderlabels = [i for i in order['Label'].drop_duplicates()]
-
-            # for i in orderlabels: 
-            #     hour_i = order.loc[order['Label']==i].head(1)['Time'].iloc[0].datetime.hour
-            #     hour_f = order.loc[order['Label']==i].tail(1)['Time'].iloc[0].datetime.hour
-            #     minute_i = order.loc[order['Label']==i].head(1)['Time'].iloc[0].datetime.minute
-            #     minute_f = order.loc[order['Label']==i].tail(1)['Time'].iloc[0].datetime.minute
-
-    
-            #     if 13 <= hour_i <= 23:
-            #         hour_i = hour_i-24
-            #     if 13 <= hour_f <= 23:
-            #         hour_f = hour_f-24
-        
-            #     ordertime.append((float(f"{hour_i}.{minute_i}"),float(f"{hour_f}.{minute_f}"))) 
-
-            
-            # for each in graphic:
-            #     if each['Label'].iloc[0] in orderlabels:
-
-            #         ALT = each['Alt'].tolist()
-            #         AZ = each['Az'].tolist()
-            #         T = each['Time'].tolist()
-
-            #         plt.scatter(T, ALT,c=AZ,cmap="viridis",lw=1)
-
-            #         for j, alt in enumerate(ALT):
-            #             plt.annotate(str(each['Label'].iloc[0]), (T[j], alt + 1), color='k')        
-
-            #         min_alt.append(min(ALT))
-            #         max_alt.append(max(ALT))
-
-            # # Limits of good observations
-            # plt.plot(time_array, [limite_altitud] * len(time_array), '--', color='grey')
-            # #Details
-            # plt.colorbar().set_label("Azimuth [deg]")
-            # plt.xlabel("Hours around Midnight")
-            # plt.ylabel("Altitude [deg]")
-
-            # # Limits of good observations
-            # for each in ordertime:
-            #     plt.plot([each[0]] * len(np.linspace(min(min_alt),max(max_alt), len(time_array))), np.linspace(min(min_alt),max(max_alt), len(time_array)), '--', color='orange')
-            #     plt.plot([each[1]] * len(np.linspace(min(min_alt),max(max_alt),len(time_array))), np.linspace(min(min_alt),max(max_alt), len(time_array)), '--', color='salmon')
-
-            #     plt.text( each[0],min(min_alt), 'S', color='orange')
-            #     plt.text( each[1],min(min_alt), 'F',color='salmon')
-
-            # #Exes
-            # hours = [str(round(num + 24,1)) if num < 0 else str(round(num,1)) for num in np.array([-6,-4,-2,0,2,4,6])]
-            # plt.xticks(np.array([-6,-4,-2,0,2,4,6]), hours)
-            # plt.title('Starting time {}\n Location: ({} , {})'.format(date_i,observer[0],observer[1]))
-            # plt.grid(True)
-
-            # Convertir la gráfica a imagen base64
-            buffer = BytesIO()
-            plt.savefig(buffer, format='png')
-            buffer.seek(0)
-            plot_url = base64.b64encode(buffer.getvalue()).decode('utf-8')
-            plt.close()  # Cerrar la figura para liberar memoria
-
 
     return render_template('planificador_astro.html',
                         error=error, 
                          datos_tabla=datos_tabla,
                          plot_url=plot_url,
-                         plot_url_altaz=plot_url_altaz,
                          año_actual=datetime.now().year)
 
 
 # Funciones astronómicas proporcionadas
 def DeltaTime(Date_i, Date_f, t_scale):
+
     scale = t_scale[0]
     sc = t_scale[1]
 
@@ -236,8 +212,14 @@ def DeltaTime(Date_i, Date_f, t_scale):
     
     return t_
 
+def CreateTime(Date_i, Date_f, t_scale):
+    time_midnight = Time(Time(Date_i, format='iso', scale='utc').iso.split()[0] + ' 00:00:00', 
+                        format='iso', scale='utc')
+    delta = DeltaTime(Date_i, Date_f, t_scale)*u.hour
+    return time_midnight + delta
+
 #Graphic
-def Graphic(Data,rango,time):
+def Graphic(Data,rango,Time):
     targets = []
 
     for i in range(1,rango):
@@ -245,7 +227,7 @@ def Graphic(Data,rango,time):
         if Target.empty:
             continue
         
-        Target['Time'] = time
+        Target['Time'] = Time
         Target = Target[Target['Observable'] != False]
 
         if not Target.empty:
@@ -256,13 +238,8 @@ def Graphic(Data,rango,time):
     
     return targets
 
-def CreateTime(Date_i, Date_f, t_scale):
-    time_midnight = Time(Time(Date_i, format='iso', scale='utc').iso.split()[0] + ' 00:00:00', 
-                        format='iso', scale='utc')
-    delta = DeltaTime(Date_i, Date_f, t_scale)*u.hour
-    return time_midnight + delta
 
-def Observations(observer, alert, Date_i, Date_f, time_scale, rango, m_min,limit):
+def Observations(observer, alert, Date_i, Date_f, time_scale, rango, limit, m_min):
     time = CreateTime(Date_i, Date_f, time_scale)
     lat_conv, lon_conv = observer
     observer_loc = EarthLocation(lat=lat_conv*u.deg, lon=lon_conv*u.deg)
@@ -270,6 +247,9 @@ def Observations(observer, alert, Date_i, Date_f, time_scale, rango, m_min,limit
     
     alert['Label'] = range(1, rango)
     alert = alert[alert['Mag'] <= m_min].copy()
+
+    if alert.empty:
+        return pd.DataFrame()
 
     for each_time in time:
         celestial_coord = SkyCoord(ra=alert['RA'], dec=alert['DEC'])
@@ -290,29 +270,38 @@ def ExpositionTime(K, m):
 
 def Order(Data, rango, Time, General_time, K):
     targets = []
-    order = []
+    order = [] 
     order_expo =[]
     finalorder = []
 
     if Data.empty:
         return 'No se encontraron observaciones'
 
-    for i in range(1, rango):
+    for i in range(1,rango):
+        #Organize and add time to the dataframe
         Target = Data.loc[Data['Label'] == i].copy()
+
+        #If theres no dataframe continue
         if Target.empty:
             continue
         
         Target['Time'] = Time
+
+        #Calculate the time exposition
         Target['Time expo'] = ExpositionTime(K, Target['Mag'])
+
+        #Do not take the targets that are not observable and the time expo is in the slot of (1-3600)s
         Target = Target[(Target['Observable'] != False) & (Target['Time expo'].between(1, 3600, inclusive='neither'))]
 
+        #Take from dataframe just the observables
         if not Target.empty:
             targets.append(Target)
 
     if not targets:
-        return pd.DataFrame()
+        return [], pd.DataFrame()
 
-    obs = pd.concat(targets, axis=0).reset_index(drop=True).drop(['Observable'], axis=1)
+    #Concate everything for a better analysis
+    obs = pd.concat(targets,axis=0).reset_index(drop=True).drop(['Observable'],axis=1)
 
     for i in range(0,len(Time)):
         priority = obs.loc[obs['Time'] == Time[i]].sort_values('Alt', ascending=False, na_position='first').head(1)
@@ -322,6 +311,7 @@ def Order(Data, rango, Time, General_time, K):
     order = pd.concat(order,axis=0).reset_index(drop=True).drop(['Az'],axis=1)
     orderlabels = [i for i in order['Label'].drop_duplicates()]
 
+    
     for each in orderlabels:
         partial_order = order.loc[order['Label'] == each]
         best_alt = partial_order.sort_values('Alt', ascending=False, na_position='first').head(1)
@@ -349,7 +339,7 @@ def Order(Data, rango, Time, General_time, K):
             finalorder.append(order_expo)
 
     finalorder = pd.concat(finalorder,axis=0).reset_index(drop=True).drop(['Alt'],axis=1)
-    
+
     return finalorder
 
 
